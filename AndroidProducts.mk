9b5bcdae861bf7b73a4ba2bf9ad72afdaac4e29b
Rohit Tiwari <rohitttiwari52@gmail.com>
2023-10-07 09:37:58 +0530

    Fleur: Lineage Bringup
    Signed-off-by: Rohit Tiwari <rohitttiwari52@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_fleur.mk
+    $(LOCAL_DIR)/lineage_fleur.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_fleur-user \
-    evolution_fleur-userdebug \
-    evolution_fleur-eng
+    lineage_fleur-user \
+    lineage_fleur-userdebug \
+    lineage_fleur-eng
--- lineage_fleur.mk
+++ lineage_fleur.mk
@@ -2,15 +2,15 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common EvolutionX stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from fleur device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := evolution_fleur
+PRODUCT_NAME := lineage_fleur
 PRODUCT_MODEL := Redmi Note 11S 4G
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
